3915d47b8f7479aa6a16052624a0c5a1e2dfb27a
hakyung <coding891@gmail.com>
2019-09-19 15:08:32 +0900

    set mongoose, passport, cookieSession, ejs, routes...

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const COOKIE_KEY = process.env.COOKIE_KEY;
+const passport = require('passport');
+require('./config/passport');
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected!');
+});
 
 const index = require('./routes/index');
+const auth = require('./routes/auth');
+const problems = require('./routes/problems');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 *1000,
+  keys: [COOKIE_KEY]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', index);
+app.use('/auth', auth);
+app.use('/problems', problems);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
