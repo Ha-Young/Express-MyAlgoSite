32b3a0628fc7b57f29e94d8c14286f21dc9430c3
park hanjun <park.hanjun@gmail.com>
2019-09-20 16:16:46 +0900

    final.. fix indenting

--- public/javascripts/problem.js
+++ public/javascripts/problem.js
@@ -1,9 +1,10 @@
-const textarea = document.getElementById('editor');
-const myCodeMirror = CodeMirror.fromTextArea(textarea, {
-  lineNumbers: true,
-  lineWrapping: true,
-  mode: 'javascript',
-  theme: 'dracula'
-});
-
-myCodeMirror.setSize('auto', 500);
+(function () {
+  const textarea = document.getElementById('editor');
+  const myCodeMirror = CodeMirror.fromTextArea(textarea, {
+    lineNumbers: true,
+    lineWrapping: true,
+    mode: 'javascript',
+    theme: 'dracula'
+  });
+  myCodeMirror.setSize('auto', 500);
+})();
