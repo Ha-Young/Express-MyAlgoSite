2c7cb40d043936414d5d5c0fdf285573b1d99f23
Simon <sijeecodes@gmail.com>
2019-09-20 16:46:38 +0900

    update codemirror on /problems

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const authCheck = require('./middlewares/checkAuth');
 const indexController = require('./controllers/index.controller');
 
--- routes/problems.js
+++ routes/problems.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const authCheck = require('./middlewares/checkAuth');
 const Problem = require('../models/Problem');
-// const indexController = require('./controllers/index.controller');
 
-/* GET home page. */
 router.get('/:problem_id', authCheck, async (req, res, next) => {
+  const problem = await Problem.findOne({ _id: req.params.problem_id });
+  res.render('problems', {
+    title: 'Vanilla',
+    user: req.user,
+    problem: problem
+  });
+});
+
+router.post('/:problem_id', authCheck, async (req, res, next) => {
   const problem = await Problem.findOne({ _id: req.params.problem_id });
   res.render('problems', { title: 'Vanilla', user: req.user, problem: problem });
 });
