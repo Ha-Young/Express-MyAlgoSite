ed2081342711b7a81a5f6987aad7f02557feeca0
hakyung <coding891@gmail.com>
2019-09-20 18:38:42 +0900

    handled error

--- routes/problems.js
+++ routes/problems.js
@@ -22,42 +22,50 @@ router.post('/:problem_id', function(req, res, next) {
     return next();
   }
   Problem.findOne({ _id: req.params.problem_id }, function(err, problem) {
-    try {
-      const code = req.body.code;
-      if (code) {
-        const result = [];
-        let isPassed = true;
-        const sandbox = {};
-        for (var i = 0; i < problem.tests.length; i++) {
-          const script = new vm.Script(`
-              ${code}
-              ${problem.tests[i].code}
-            `);
-          const context = vm.createContext(sandbox);
-          const invoke = script.runInContext(context, { timeout: 500 });
-          if (invoke === problem.tests[i].solution) {
-            result.push({
-              isAnswer: true,
-              insteadGot: String(invoke),
-              expected: problem.tests[i].solution
-            });
-          } else {
-            isPassed = false;
-            result.push({
-              isAnswer: false,
-              insteadGot: String(invoke),
-              expected: problem.tests[i].solution
-            });
+    if (!err) {
+      try {
+        const code = req.body.code;
+        if (code) {
+          const result = [];
+          let isPassed = true;
+          const sandbox = {};
+          for (var i = 0; i < problem.tests.length; i++) {
+            const script = new vm.Script(`
+                ${code}
+                ${problem.tests[i].code}
+              `);
+            const context = vm.createContext(sandbox);
+            const invoke = script.runInContext(context, { timeout: 500 });
+            if (invoke === problem.tests[i].solution) {
+              result.push({
+                isAnswer: true,
+                insteadGot: String(invoke),
+                expected: problem.tests[i].solution
+              });
+            } else {
+              isPassed = false;
+              result.push({
+                isAnswer: false,
+                insteadGot: String(invoke),
+                expected: problem.tests[i].solution
+              });
+            }
           }
+          isPassed
+            ? res.render('success')
+            : res.render('failure', { error: null, result, user: req.user });
+        } else {
+          res.render('failure', {
+            error: 'solution이 없습니다!',
+            result: [],
+            user: req.user
+          });
         }
-        isPassed
-          ? res.render('success')
-          : res.render('failure', { error: null, result });
-      } else {
-        res.render('failure', { error: 'solution이 없습니다!', result: [] });
+      } catch (err) {
+        res.render('failure', { error: err.stack, result: [], user: req.user });
       }
-    } catch (err) {
-      res.render('failure', { error: err.stack, result: [] });
+    } else {
+      next(err);
     }
   });
 });
