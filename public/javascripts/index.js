873575539299ddc7f21196bd622338eb761e0d33
Jeong <inyeop0212@gmail.com>
2020-03-09 17:31:13 +0900

    Render problemDetail.ejs #2

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,12 @@
-console.log('hello world!');
+const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+  mode: {name: "changes"},
+  lineNumbers: true,
+  indentUnit: 4
+});
+const form = document.getElementById("editor-form");
+
+form.addEventListener('submit', (e) => {
+  if(!confirm('제출하시겠습니까?')) {
+    e.preventDefault();
+  }
+});
