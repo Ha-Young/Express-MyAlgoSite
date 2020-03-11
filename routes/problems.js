a0726b6e327f3c4ddcd66ef4c7242f48bad542ba
nathaneast <ahrl_ahrl@naver.com>
2020-03-11 23:27:18 +0900

    문제 상세페이지 form 작업중

--- routes/problems.js
+++ routes/problems.js
@@ -4,16 +4,21 @@ const Problem = require('../models/Problem');
 
 router.get('/:problem_id', async (req, res) => {
   try {
-    const problem = await Problem.findOne({ id: req.params.problem_id});
+    const problem = await Problem.findOne({ id: req.params.problem_id });
 
     res.render('problem', {
       title: "problem",
       user: req.user.username,
       problem
     });
-  } catch(err) {
+  } catch (err) {
     console.error(err);
   }
+});
+
+router.post('/:problem_id', async (req, res) => {
+  const problem = await Problem.findOne({ id: req.params.problem_id });
+  console.log(222222222222222222222222222222222222222222222222222, req)
 });
 
 module.exports = router;
