3e076bf713c3f6ae6bf64f4f4044bccd429b1aef
Jeong <inyeop0212@gmail.com>
2020-03-12 16:10:38 +0900

    Update UI logic

--- models/User.js
+++ models/User.js
@@ -6,7 +6,11 @@ const mongoose = require('mongoose');
 
  */
 const userSchema = new mongoose.Schema({
-  githubId: String
+  githubId: String,
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
