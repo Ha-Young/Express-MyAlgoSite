96bac1059e064073db8afd149dbb936bcfc33af7
unknown <760kry@gmail.com>
2020-03-12 16:50:24 +0900

    failure.ejs 구현 중

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -11,12 +11,23 @@ exports.isLoggedIn = (req, res, next) => {
 }
 
 exports.isSeccesss = (req, res, next) => {
-
+    
     const problem = sample_problems.filter((el) => {
         return el.id === Number(req.params.problem_id)
     })[0];
-
+    
     const problemTestList = problem.tests;
+    
+    try {
+        new vm.Script(req.body.submitValue);
+    } catch (err) {
+        if(err) {
+            return res.render('failure', {
+                failmessage: err,
+                error: err.stack
+            })
+        }
+    }
 
     const script = new vm.Script(req.body.submitValue);
 
@@ -26,7 +37,7 @@ exports.isSeccesss = (req, res, next) => {
         script.runInNewContext(context);
     });
 
-    let sulutionFn = Object.keys(contexts[0])[0];
+    let solutionFn = Object.keys(contexts[0])[0];
 
     let testValue = [];
 
@@ -37,8 +48,11 @@ exports.isSeccesss = (req, res, next) => {
     }
 
     for (let i = 0; i < testValue.length; i++) {
-        if (contexts[0][sulutionFn](testValue[i][0]) !== testValue[i][1]) {
-            res.render('failure');
+        if (contexts[0][solutionFn](testValue[i][0]) !== testValue[i][1]) {
+            return res.render('failure', {
+                failmessage: `${solutionFn}(${testValue[i][0]})결과값은 ${contexts[0][solutionFn](testValue[i][0])}이 아니라 ${testValue[i][1]}어야 합니다`,
+                error: ''
+            });
         }
     }
     next();
