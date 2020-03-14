67a39bce5565f09d556372e6287a25b46405ceb8
Joy Park <soodang@Joyui-MacBookPro.local>
2020-03-14 17:24:02 +0900

    주석제거

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const sampleProblems = require("../models/sample_problems");
-// TODO✅: ❓json 파일을 가지고 오는 방법? => 일반 require 방식과 같음
 
 const Problem = require("../models/Problem");
-/* GET home page. */
 router.get("/", async (req, res, next) => {
   let problems = await Problem.find();
   if (!problems.length) {
--- routes/problem.js
+++ routes/problem.js
@@ -4,20 +4,17 @@ const path = require("path");
 const vm = require("vm");
 
 router.get("/:id", async (req, res) => {
-  // 그 아이디에 맞는 문제의 정보를 디비에서 가지고 온다.
   const problem = await Problem.findOne({ id: req.params.id });
   res.render("problem", { problem: problem });
 });
 
 router.post("/:id", async (req, res) => {
-  // post 해온 코드 확인하기
   console.log(req.body);
-  //테스트 코드 불러오기
   const problem = await Problem.findOne({ id: req.params.id });
   let tests = problem.tests;
   const results = tests.map(test => {
     const context = { result: null, error: null };
-    vm.createContext(context); // Contextify the object.
+    vm.createContext(context); 
     const code =
       req.body.solutionCode +
       "\ntry{result=" +
