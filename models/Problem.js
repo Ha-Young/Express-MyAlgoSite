04a9d7b6525e32aa953eec811cf83b8225f0aaa9
Gyeop <yanggieop@gmail.com>
2020-03-15 17:11:21 +0900

    기본 스키마 수정

--- models/Problem.js
+++ models/Problem.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const ProblemSchema = new mongoose.Schema({
-  id: { type: Number, required: true },
+  id: { type: Number, required: true, unique: true },
   title: { type: String, required: true },
   completed_users: { type: Number, required: true },
-  difficulty_level: { type: Number, required: true },
+  difficulty_level: { type: Number, required: true, enum: [1, 2, 3, 4] },
   description: { type: String, required: true },
   default_function: { type: String, required: true },
-  tests: { type: Array, required: true }
+  tests: [{ code_params: mongoose.Schema.Types.Mixed, solution: mongoose.Schema.Types.Mixed }]
 });
 
 module.exports = mongoose.model('Problem', ProblemSchema);
--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  githubId: { type: Number, required: true },
+  githubId: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   avatarUrl: { type: String, required: true }
 });
 
