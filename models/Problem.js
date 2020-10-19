12e46bcb27397ff15a9ee790c54de7ef86f65434
Soin Na <carrotcold@gmail.com>
2020-10-19 21:24:59 +0900

    feat: define schema, encrypt user password using bcrypt

--- models/Problem.js
+++ models/Problem.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 
-/*
-
-  TODO: Fill in the model specification
-
- */
 const ProblemSchema = new mongoose.Schema({
-
+  id: Number,
+  title: {
+    type: String,
+    required: true,
+  },
+  completed_users: {
+    type: Number,
+    default: 0,
+  },
+  difficulty_level: {
+    type: Number,
+    default: 1,
+  },
+  description: String,
+  tests: [{ code: String, solution: String }],
 });
 
 module.exports = mongoose.model('Problem', ProblemSchema);
--- models/User.js
+++ models/User.js
@@ -1,12 +1,35 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-/*
-
-  TODO: Fill in the model specification
-
- */
-const userSchema = new mongoose.Schema({
-
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+UserSchema.pre('save', async function (next) {
+  const user = this;
+
+  try {
+    const encryptedPassword = await bcrypt.hash(user.password, saltRounds);
+    user.password = encryptedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
