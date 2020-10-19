d74a1996a6a9a11d35a76b846e36160fc48a96f3
부트캠프 <mini_16@vanillacoding-Macmini-16.local>
2020-10-19 14:11:49 +0900

    feat: add login route

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express');
-
+const login = require('./routes/login');
 const index = require('./routes/index');
 
 const app = express();
 
 app.use('/', index);
-
+app.use('/login', login);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
