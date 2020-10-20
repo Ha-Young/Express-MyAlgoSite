606b2f59e0bb164ab3cc935f03e45ab97e6ee2da
gyeongwoopark <geewoo94@gmail.com>
2020-10-20 16:24:37 +0900

    STYLE: code style

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-if (process.env.NODE_ENV === 'development') require('dotenv').config();
+/* eslint-disable no-unused-vars */
+if (process.env.NODE_ENV === "development") require("dotenv").config();
 
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-const dbConnection = require('./mongodb');
-const passport = require('./passport/github');
+const dbConnection = require("./mongodb");
+const passport = require("./passport/github");
 
-const router = require('./routes/index');
+const router = require("./routes/index");
 
 const app = express();
 
-dbConnection.once('open', (err) => {
+dbConnection.once("open", (err) => {
   if (err) throw Error(err);
 
-  console.log('mongodb is connected...');
+  console.log("mongodb is connected...");
 });
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cookieParser());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(router);
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
