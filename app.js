e3601c33ef71e3c72ddacbcf8f36e4acf51b9048
justindglee <nninnnin7@gmail.com>
2020-10-21 16:56:00 +0900

    style: edit mongo connect message

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, () => {
-  console.log('mongod connected!')
+  console.log('mongodb is connected!');
 });
 
 app.set('views', __dirname + '/views');
