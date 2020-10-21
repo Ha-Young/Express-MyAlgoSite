7ef399e89f40d44bf5a05f1af0c4753cc97db447
justindglee <nninnnin7@gmail.com>
2020-10-21 16:06:48 +0900

    style: add code mirror theme, adjust tab size and font size

--- utils/codeMirror.js
+++ utils/codeMirror.js
@@ -2,6 +2,9 @@ const myTextarea = document.getElementById('myTextarea');
 const editor = CodeMirror.fromTextArea(myTextarea, {
   lineNumbers: true,
   mode: "javascript",
+  tabSize: 2,
+  theme: 'hopscotch'
 });
 
+editor.getDoc().setValue('function solution (x) {\r\n\t\r\n}\r\n');
 editor.save();
