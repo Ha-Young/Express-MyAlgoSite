cdbada2e19ca14b5a6c06b8bb70a4d069c8a1485
Chan <coin46@naver.com>
2020-10-23 18:11:09 +0900

    Modify. Modify callbackurl

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ module.exports = () => {
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: 'http://localhost:3000/auth/github/callback',
+        callbackURL: 'https://afternoon-sea-45834.herokuapp.com/auth/github/callback',
       },
       async function (accessToken, refreshToken, profile, cb) {
         const { id, username, provider } = profile;
