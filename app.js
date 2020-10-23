5c3a406c9004826b418aa97df11b5de29ba11c72
justindglee <nninnnin7@gmail.com>
2020-10-23 16:51:54 +0900

    refactor: conditionally import and adjust dotenv in app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const dotenv = require('dotenv');
-dotenv.config();
+if (process.env.NODE_ENV === 'development') {
+  const dotenv = require('dotenv');
+  dotenv.config();
+}
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
