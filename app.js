60973412550b8a29ab11705f306d3a347d5c9188
woori9 <woori9@users.noreply.github.com>
2020-10-23 18:44:47 +0900

    제출

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ const session = require('express-session');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const flash = require('express-flash');
 require('dotenv').config();
-
 const MONGO_URI = process.env.MONGOURI;
 const SESSION_SECRET = process.env.SESSION_SECRET;
-
 
 try {
   mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,33 +16,21 @@ try {
   next(err);
 }
 
-
-
 const app = express();
-console.log("EXPRESS",app.get('env'));
-console.log("process.env.NODE_ENV", process.env.NODE_ENV);
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// getting information from forms ->
-// we want to use express.urlencoded()
-// take the form, and build a access them inside 'req' inside POST method
-
 app.use(flash());
-app.use(session({//passport는 내부적으로 session을 사용하기 떄문에 기본적인 session설정을 필요합니다.
+app.use(session({
   secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }));
-app.use(passport.initialize());//passport 동작
-app.use(passport.session());//want variables to be persisted across the entire session
-//passport.session()은 라우터로 이동할 때 마다 실행되는데,
-//이때 passport.deserializeUser을 통해서 사용자 인증을 할 수 있습니다.
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', index);
 
 // catch 404 and forward to error handler
@@ -58,9 +43,6 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  console.log("req", req.app.get('env'))//npm start를 하면 production / npm dev run이면 development
-  console.log(err);
-  console.log(err.message)//NOT FOUND
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
--- routes/index.js
+++ routes/index.js
@@ -10,18 +10,16 @@ const localStrategy = require('passport-local').Strategy;
 
 passport.use(new localStrategy({ usernameField: 'email', passwordField: "password" },
   async function (email, password, done) {
-    const user = await Users.find({ email: email }).exec();
+    const user = await Users.find({ email }).exec();
     if (user.length === 0) {
       return done(null, false, { message: 'No user with this email address' });
     }
 
     try {
       const validPassword = await bcrypt.compare(password, user[0].password);
-      if (validPassword) {
-        return done(null, user[0]);
-      } else {
-        return done(null, false, { message: 'password incorrect' });
-      }
+
+      if (validPassword) return done(null, user[0]);
+      return done(null, false, { message: 'password incorrect' });
     } catch (err) {
       return done(err);
     }
@@ -29,20 +27,16 @@ passport.use(new localStrategy({ usernameField: 'email', passwordField: "passwor
 ));
 
 passport.serializeUser((user, done) => {
-  console.log("serialize", user);
   done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-  const a = await Users.find({ id: id }).exec();
+  const a = await Users.find({ id }).exec();
   return done(null, a[0]);
 });
 
 /* GET home page. */
 router.get('/', checkAuthenticated, async (req, res, next) => {
-  //console.log(res.getHeader('content-type'));
-  //console.log("session", req.session);
-  //console.log("from deserialize", req.user);//from deserialize...req.user
   try {
     const problems = await Problems.find({});
     res.render('index', { items: problems });
@@ -58,14 +52,15 @@ router.get('/problem/:problem_id', checkAuthenticated, async (req, res, next) =>
 
     if (targetProblem.length === 0) {
       res.redirect('/');
-    } else {
-      if (userHistory.length > 0) {
-        const codeWritten = userHistory[0].codeWritten;
-        res.render('problem', { targetProblem, codeWritten });
-      } else {
-        res.render('problem', { targetProblem, codeWritten: undefined });
-      }
+      return;
     }
+    if (userHistory.length > 0) {
+      const codeWritten = userHistory[0].codeWritten;
+      res.render('problem', { targetProblem, codeWritten });
+    } else {
+      res.render('problem', { targetProblem, codeWritten: undefined });
+    }
+
   } catch (err) {
     next(err);
   }
@@ -77,6 +72,7 @@ router.post('/problem/:problem_id', checkAuthenticated, async (req, res, next) =
 
   try {
     const failedTest = {};
+
     for (let i = 0; i < targetProblem[0].tests.length; i++) {
       const code = targetProblem[0].tests[i].code;
       const solution = targetProblem[0].tests[i].solution;
@@ -85,6 +81,7 @@ router.post('/problem/:problem_id', checkAuthenticated, async (req, res, next) =
       const script = new vm.Script(executtionScript);
       vm.createContext(context);
       script.runInContext(context);
+
       if (context.result !== solution) {
         failedTest[targetProblem[0].tests[i].code] = targetProblem[0].tests[i].solution;
         if (i === targetProblem[0].tests.length - 1) {
@@ -101,19 +98,17 @@ router.post('/problem/:problem_id', checkAuthenticated, async (req, res, next) =
           });
           await newUser.save();
           res.render('success');
-
         } catch (err) {
           next(err);
         }
       }
     }
   } catch (err) {
-    res.render('error', { message: "There is error in your code", error: { stack: err } })
+    res.render('error', { message: "There is error in your code", error: { stack: err } });
   }
 });
 
 router.get('/login', (req, res, next) => {
-  console.log("session", req.session);
   res.render('login');
 });
 
@@ -124,7 +119,6 @@ router.post('/login', passport.authenticate('local', {
 }));
 
 router.get('/register', (req, res, next) => {
-  console.log("session", req.session);
   res.render('register');
 });
 
@@ -145,11 +139,7 @@ router.post('/register', async (req, res, next) => {
 });
 
 function checkAuthenticated(req, res, next) {
-  console.log("checkAuthenticated", req.session);
-  console.log(req.isAuthenticated())
-  if (req.isAuthenticated()) {
-    return next();
-  }
+  if (req.isAuthenticated()) return next();
   res.redirect(302, '/login');
 }
 
