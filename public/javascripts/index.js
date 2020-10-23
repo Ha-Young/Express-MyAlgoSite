39c8e0b00acde19fc6ae94730cafe0cca07288f5
부트캠프 <mini_16@vanillacoding-Macmini-16.local>
2020-10-23 18:37:55 +0900

    delete: codemirror themes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,8 @@
 /* CodeMirror 3.14.2 .....*/
 let codemirror = new CodeMirror.fromTextArea(document.getElementById("codemirror"), {
-  mode: "javascript",
+  mode: 'javascript',
   lineNumbers: true,
   lineWrapping: true,
   tabSize: 2,
-  theme: "material-darker",
+  theme: 'material-darker',
 });
