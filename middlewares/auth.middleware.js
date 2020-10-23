b1c417a58de4f4131d4bc9f87cbbca156bd261e2
dohee3520 <60972250+dohee3520@users.noreply.github.com>
2020-10-23 14:33:35 +0900

    refactor: refactoring code

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 const authenticateUser = (req, res, next) => {
   const isLoggedIn = req.isAuthenticated();
 
-  isLoggedIn ? next() : res.status(301).redirect('/login');
+  isLoggedIn ? next() : res.status(200).redirect('/login');
 };
 
 module.exports = authenticateUser;
