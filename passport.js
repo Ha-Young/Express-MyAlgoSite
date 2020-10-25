66bbce72ce1613adda63b7e4775c3698a5cca6bf
Crystal Hwang <crystal.hwang007@gmail.com>
2020-10-25 16:32:28 +0900

    Fix : Modify callbackURL

--- passport.js
+++ passport.js
@@ -10,8 +10,7 @@ const User = require('./models/User');
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  //callbackURL: "https://intense-reef-27088.herokuapp.com/login/auth/github/callback"
-  callbackURL: "http://localhost:3000/login/auth/github/callback"
+  callbackURL: "https://intense-reef-27088.herokuapp.com/login/auth/github/callback"
 },
   async function (accessToken, refreshToken, profile, cb) {
     const findOrCreateUserInfo = async (profile) => {
