73c50db8f8b231d342958e34780a24320600c355
Ethan Shin <shinhc5@gmail.com>
2020-10-25 17:53:32 +0900

    refactor: update Schema (Problem, User)

--- models/Problem.js
+++ models/Problem.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.SchemaTypes.ObjectId;
+const Mixed = mongoose.SchemaTypes.Mixed;
 
 const ProblemSchema = new mongoose.Schema({
   title: { type: String, required: true },
   completed_users: { type: Number, required: true },
   difficulty_level: { type: Number, required: true },
   description: { type: String, required: true },
-  tests: [Object],
+  tests: [
+    {
+      code: { type: String, required: true },
+      solution: { type: Mixed, required: true }
+    }
+  ],
   completed_user_ids: [{ type: ObjectId, ref: 'User' }]
 });
 
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  github_id: { type: String, required: true },
+  github_id: { type: String, required: true, unique: true },
   display_name: { type: String, required: true },
   username: { type: String, required: true },
   profile_url: String,
