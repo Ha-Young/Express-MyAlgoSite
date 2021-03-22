30fc34dc1e900f21a985d43e6d57f6ea1c38e27f
hee-c <ways2kim@gmail.com>
2021-03-23 01:44:26 +0900

    Add problem_id(POST) run untrusted code in sandbox

--- routes/problems.js
+++ routes/problems.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { NodeVM } = require('vm2');
 const router = express.Router();
 const { verifyUser } = require('./middlewares/verifyUser');
 const Problem = require('../models/Problem');
@@ -8,17 +9,49 @@ router.get('/', verifyUser, (req, res, next) => {
 });
 
 router.get('/:problem_id', verifyUser, async (req, res, next) => {
-  console.log('in problem_id')
   const targetProblemId = parseInt(req.params['problem_id']);
 
   const targetProblem = await Problem.findOne({ id: targetProblemId });
-  console.log(targetProblemId)
-  console.log(targetProblem)
+
   res.status(200).render('problem', { problem: targetProblem });
 });
 
-router.post('/:problem_id', verifyUser, (req, res, next) => {
-  
+router.post('/:problem_id', verifyUser, async (req, res, next) => {
+  const targetProblemId = parseInt(req.params['problem_id']);
+  const targetProblem = await Problem.findOne({ id: targetProblemId });
+  const userSolution = req.body.solution;
+  const judgeResult = {};
+
+  const vm = new NodeVM({
+      console: 'inherit',
+      sandbox: {
+        tests: targetProblem.tests,
+        judgeResult,
+      },
+      require: {
+        builtin: ['*'],
+      },
+  });
+
+  try {
+    vm.run(`
+      ${userSolution}
+
+      for (const test of tests) {
+        console.log(eval(test.code))
+        console.log(test.solution)
+        if (eval(test.code) !== test.solution) {
+          judgeResult[test.code] = false;
+        } else {
+          judgeResult[test.code] = true;
+        }
+      }
+    `);
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.render('success', { solution: userSolution })
 });
 
 module.exports = router;
