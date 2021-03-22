6f477a89e424cc5de58dd665fad779ede6389556
wonho <biocle8339@gmail.com>
2021-03-22 14:21:55 +0900

    ADD dotenv, mongoose, .env file DELETE body-parser

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const index = require('./routes/index');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
