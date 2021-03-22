b185678daa1426d687c9ddcbe06155460b5f7f80
hee-c <ways2kim@gmail.com>
2021-03-22 14:15:11 +0900

    Init project

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-const express = require('express');
+require('dotenv').config();
 
-const index = require('./routes/index');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => console.log('Connected to mongoDB server'));
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const publicDirectoryPath = path.join(__dirname, './public');
+const viewsDirectoryPath = path.join(__dirname, './views');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', viewsDirectoryPath);
+
+app.use(express.static(publicDirectoryPath));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+const index = require('./routes/index');
+
 app.use('/', index);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
