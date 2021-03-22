e37a069dc9bdb5fbeae82c4dae60b891c38330fc
yobe8 <sanyobe@gmail.com>
2021-03-22 15:59:40 +0900

    Add middlewares

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const express = require('express');
+require("dotenv").config();
 
-const index = require('./routes/index');
+const { ROOT_DIR_NAME } = require(`${__dirname}/constants/constants`);
+
+const express = require('express');
+const morgan = require("morgan");
+const fs = require("fs");
+
+const index = require(`${ROOT_DIR_NAME}/routes`);
 
 const app = express();
+
+app.set("view engine", "ejs");
+
+const logStream = fs.createWriteStream(`${ROOT_DIR_NAME}/log/app.log`, { flags: "a" });
+
+app.use(morgan("combined", { stream: logStream }));
+app.use(express.static("public"));
 
 app.use('/', index);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ constants/constants.js
@@ -0,0 +1,3 @@
+const ROOT_DIR_NAME = `${__dirname}/..`;
+
+exports.ROOT_DIR_NAME = ROOT_DIR_NAME;
