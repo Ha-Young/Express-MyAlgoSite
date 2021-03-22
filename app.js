55c993eb740987f296f21afe5cd9a95796b25261
onemo <wonmotime1@naver.com>
2021-03-22 17:14:41 +0900

    add mongoatlas

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Database connection sucessful👍🏻'))
+  .catch(err => console.log(err));
 
 const index = require('./routes/index');
+const loginRouter = require('./routes/login');
 
 const app = express();
 
-app.use('/', index);
-
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
+
+app.use('/', index);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
