8565e64e95d452f79f4d795085eb4ef5d441f659
ha-young <hayeong28@naver.com>
2021-03-22 20:52:29 +0900

    add: initial styling on layout header, footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('pages/index', { title: '바닐라코딩' });
+  res.render('pages/index');
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('pages/login');
 });
 
 module.exports = router;
