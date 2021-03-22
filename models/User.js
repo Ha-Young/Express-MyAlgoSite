876dd12433b8c0e1fc4b42a0218b09e3d0393cc8
김탁현 <kimtakhyun@gimtaghyeon-ui-MacBook-Pro.local>
2021-03-22 16:02:33 +0900

    Add the membership registration rogic according to member information

--- models/User.js
+++ models/User.js
@@ -6,8 +6,8 @@ const mongoose = require("mongoose");
 
  */
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  username: String,
+  password: String,
 });
 
 module.exports = mongoose.model("User", userSchema);
--- routes/controllers/register.controller.js
+++ routes/controllers/register.controller.js
@@ -1,8 +1,24 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require("../../models/User");
 
 exports.register = async (req, res) => {
   const { username, password } = req.body;
   const exist = await User.findOne({ username });
+
+  if (exist) {
+    res.status(504).send("이미 존재하는 아이디입니다.");
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({
+    username,
+    hashedPassword,
+  });
+
+  await user.save();
+  res.redirect('/');
 };
