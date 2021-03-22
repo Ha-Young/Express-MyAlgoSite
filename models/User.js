7fdacad15591abe2232f2d6cf5c23adf17050908
ParkJeongbeen <pjb6510@gmail.com>
2021-03-22 22:39:10 +0900

    add userSchema.pre(save)

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 /*
 
@@ -6,7 +7,33 @@ const mongoose = require('mongoose');
 
  */
 const userSchema = new mongoose.Schema({
-
+  name: { type: String, maxlength:20, require: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  token: { type: String },
 });
 
-module.exports = mongoose.model('User', userSchema);
+const saltRounds = 10;
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
