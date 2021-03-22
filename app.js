60a1db350f09898e6b4e69eb797dcfa8eecb492a
a01081440011 <a01081440011@gamil.com>
2021-03-22 16:50:47 +0900

    Set initalization

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-const express = require('express');
+const express = require("express");
+const createError = require("http-errors");
+const bodyParser = require("body-parser");
 
-const index = require('./routes/index');
+const index = require("./routes/index");
 
 const app = express();
 
-app.use('/', index);
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.static(`${__dirname}/public`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404, "Not Found"));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
