131d0b7bb2693550ee0568a35162bda8676daeb9
Jay_WooKJ <natcho9010@gmail.com>
2021-03-22 16:18:18 +0900

    schema done

--- models/Problem.js
+++ models/Problem.js
@@ -1,12 +1,36 @@
 const mongoose = require('mongoose');
 
-/*
+const TestCase = new mongoose.Schema({
+  testcase: mongoose.Mixed,
+  answer: mongoose.Mixed,
+});
 
-  TODO: Fill in the model specification
+const Challenger = new mongoose.Schema({
+  _id: mongoose.ObjectId,
+  solved: Boolean,
+});
 
- */
+const Accuracy = new mongoose.Schema({
+  accuracy: Number,
+  challengers_count: Number,
+  challengers: [Challenger],
+});
+
 const ProblemSchema = new mongoose.Schema({
-
+  _id: {
+    type: mongoose.ObjectId,
+    index: true,
+  },
+  title: String,
+  description: String,
+  difficuty_level: Number,
+  author: String,
+  tags: [String],
+  testcases: [TestCase],
+  like: [mongoose.ObjectId],
+  accuracy: {
+    type: Accuracy,
+  },
 });
 
 module.exports = mongoose.model('Problem', ProblemSchema);
--- models/User.js
+++ models/User.js
@@ -1,12 +1,41 @@
 const mongoose = require('mongoose');
 
-/*
+const problemsSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.ObjectId,
+    index: true,
+  },
+  score: Number,
+  solved: Boolean,
+  code: String,
+});
 
-  TODO: Fill in the model specification
-
- */
 const userSchema = new mongoose.Schema({
-
+  _id: {
+    type: mongoose.ObjectId,
+    index: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+    match: /(\b.+\@.+\..+\b)/,
+  },
+  name: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  exprience: Number,
+  kyu: Number,
+  problems: [problemsSchema],
 });
 
 module.exports = mongoose.model('User', userSchema);
