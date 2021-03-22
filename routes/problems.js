39106a7a6d7d25f74663d97639863d4eb90b30d8
kwon, minho <minhob38@gmail.com>
2021-03-22 19:57:34 +0900

    Implement Require3

--- routes/problems.js
+++ routes/problems.js
@@ -4,8 +4,21 @@ const problems = require("../models/sample_problems.json");
 
 router.get("/:problem_id", (req, res, next) => {
   const { problem_id } = req.params;
-  console.log(problem_id);
-  res.render("problems", { problems: problems });
+
+  // util 함수로 만들것!
+  const problemIdx = problems.findIndex((problem) => {
+    return problem.id.toString() === problem_id;
+  });
+
+  if (problemIdx === -1) {
+    //http error로 예외처리하기...
+    //next()
+  }
+
+  res.render("problems", { problem: problems[problemIdx] });
 });
 
+router.post("/", (req, res, next) => {
+  res.send("POST 구현해야 합니다.");
+});
 module.exports = router;
