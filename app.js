33840a818fc068972a87d5efbc4025b30e00e2b9
김탁현 <kimtakhyun@gimtaghyeon-ui-MacBook-Pro.local>
2021-03-22 14:09:18 +0900

    Add the middleware for show index.ejs

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const index = require("./routes/index");
 
 const app = express();
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/", index);
 
