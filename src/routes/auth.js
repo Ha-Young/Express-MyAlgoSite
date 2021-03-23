ae356a870168ab38682109d87086e72ee54b0b24
ha-young <hayeong28@naver.com>
2021-03-23 16:11:12 +0900

    add: join controller get req.body

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,9 @@
 const AuthController = require("../controllers/auth");
 
 module.exports = function authRouter(app) {
-  app.get('/sign', AuthController.viewJoin);
+  app.get('/join', AuthController.viewJoin);
   app.get('/login', AuthController.viewLogin);
 
-  app.post('/sign', AuthController.join);
+  app.post('/join', AuthController.join);
   app.post('/login', AuthController.login);
 };
