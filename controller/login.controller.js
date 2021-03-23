44dcf526c54ed2dcc0cea9c7db66d66856ec7176
ParkJeongbeen <pjb6510@gmail.com>
2021-03-23 21:31:48 +0900

    add try catch in controller

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -1,7 +1,12 @@
 const passport = require("passport");
+const createError = require("http-errors");
 
 exports.get = async (req, res, next) => {
-  res.render("login");
+  try {
+    res.render("login");
+  } catch (err) {
+    next(createError(500, err.message));
+  }
 };
 
 exports.post = passport.authenticate("local", {
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const Problem = require("../models/Problem")
+const createError = require("http-errors");
+
+const Problem = require("../models/Problem");
 
 /* GET home page. */
 
 router.get("/", async (req, res, next) => {
-  const user = await req.user;
-  const problems = await Problem.find();
+  try {
+    const user = await req.user;
+    const problems = await Problem.find();
 
-  res.render("index", {
-    email: user.email,
-    problemList: problems,
-  });
+    res.render("index", {
+      email: user.email,
+      problemList: problems,
+    });
+  } catch (err) {
+    next(createError(500, err.message));
+  }
 });
 
 module.exports = router;
