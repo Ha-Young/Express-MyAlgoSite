3c8f4c94fe11914e39e8794bff41fb2bf6790c1e
kwon, minho <minhob38@gmail.com>
2021-03-23 16:56:48 +0900

    connect mongoDB

--- models/User.js
+++ models/User.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://minho:6M7xz4E5uB-Wuka@cluster0.ehapl.mongodb.net/test",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+);
 
+const db = mongoose.connection;
+db.on("error", () => console.log("MongoDB Connection Error"));
+db.once("open", () => console.log("MongoDB Connected"));
 /*
 
   TODO: Fill in the model specification
 
  */
-const userSchema = new mongoose.Schema({
 
-});
+const userSchema = new mongoose.Schema({});
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
 
 passport.use(
   new GoogleStrategy(
@@ -10,12 +10,13 @@ passport.use(
       clientID:
         "613607891988-e2qrlnof06t5us7q7i30og3o09tmh6q4.apps.googleusercontent.com",
       clientSecret: "pDYMU2nLPASvYGsY0nwq9QI8",
-      callbackURL: "authcallback",
+      callbackURL: "login/authcallback",
     },
     function (accessToken, refreshToken, profile, cb) {
-      User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user);
-      });
+      console.log("callback");
+      // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
     },
   ),
 );
@@ -30,6 +31,10 @@ router.get(
     scope: ["profile"],
   }),
 );
+
+router.get("/authcallback", passport.authenticate("google"), (req, res) => {
+  res.send("redirect");
+});
 
 router.get("/github", (req, res, next) => {
   // handle with passport
