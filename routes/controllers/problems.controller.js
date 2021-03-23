12da6e89d5a7be7e9df2d3988a723817c16c29ce
김탁현 <kimtakhyun@gimtaghyeon-ui-MacBook-Pro.local>
2021-03-23 21:17:03 +0900

    Add the checkUserCode logic used by vm sandbox module and Add the success, failure page

--- routes/controllers/problems.controller.js
+++ routes/controllers/problems.controller.js
@@ -1,3 +1,5 @@
+const { NodeVM } = require("vm2");
+
 const Problem = require("../../models/Problem");
 
 exports.getAll = async function (req, res, next) {
@@ -17,13 +19,39 @@ exports.checkCode = async function (req, res, next) {
   const problemId = req.params.problem_id;
   const submitText = req.body.submit_text;
 
-  try {
-    const userCode = new Function("return " + submitText)();
+  const vm = new NodeVM({
+    console: 'inherit',
+    sandbox: {},
+  });
 
+  const userCode = vm.run(`module.exports = ${submitText};`);
+
+  try {
     const problem = await Problem.findById(problemId);
     const testCodes = problem.tests;
+    let count = 0;
 
-    console.log(testCodes);
+    for (let i = 0; i < testCodes.length; i++) {
+      const testCode = testCodes[i].code;
+      const correctValue = testCodes[i].solution;
+
+      const result = vm.run(
+        `const solution = ${userCode};
+        module.exports = ${testCode};`
+      );
+
+      if (result !== correctValue) {
+        return res.render("failure", { failProblem: testCode });
+      }
+
+      count++;
+    }
+
+    if (count === testCodes.length) {
+      console.log("성공이다.");
+      return res.render("success");
+    }
+
   } catch (error) {
     console.log(error.message);
   }
