ac1c69fb6b7f94398ba481db62de430625b7b5d9
ParkJeongbeen <pjb6510@gmail.com>
2021-03-23 19:56:49 +0900

    fix index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const problemSample = require("../models/sample_problems.json");
+
 /* GET home page. */
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   const user = await req.user;
-  res.render('index', { email: user.email });
+  res.render("index", {
+    email: user.email,
+    problems: problemSample,
+  });
 });
 
 module.exports = router;
