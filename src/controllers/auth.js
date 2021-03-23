648fa90f5c96c88934735daa9aacc454fc95a741
ha-young <hayeong28@naver.com>
2021-03-23 10:45:24 +0900

    add: controller layer home, auth

---
+++ src/controllers/auth.js
@@ -0,0 +1,15 @@
+exports.viewJoin = function (req, res, next) {
+  res.render('pages/join');
+};
+
+exports.viewLogin = function (req, res, next) {
+  res.render('pages/login');
+};
+
+exports.join = function (req, res, next) {
+
+};
+
+exports.login = function (req, res, next) {
+
+};
