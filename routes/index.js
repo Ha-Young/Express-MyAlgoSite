db45e48f0413e49af2efec9d9351d576ba955dc9
eluka22 <eluka22@gmail.com>
2021-03-23 15:34:40 +0900

    Add: authorization and problems routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("./middlewares/authorization");
+const { isAuth } = require("./middlewares/authorization");
+const problemsData = require("../models/sample_problems.json");
 
 /* GET home page. */
-router.get('/', verifyToken, (req, res, next) => {
-  res.render('index', { title: '바닐라코딩' });
+router.get("/", isAuth, (req, res, next) => {
+  res.render("index", {
+    problems: problemsData
+  });
 });
+
+router.get("/problems/:problem_id", (req, res, next) => {
+  const id = parseInt(req.params.problem_id);
+  const [ problem ] = problemsData.filter(problem => problem.id === id);
+
+  res.render("problem", { problem });
+})
 
 module.exports = router;
