f89b9c67286c581def1c58bc6e870ae76b14e226
ParkJeongbeen <pjb6510@gmail.com>
2021-03-23 16:30:39 +0900

    add loginController

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const loginController = require("../controller/login.controller");
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.render("login");
-});
+router.get("/", loginController.get);
+router.post("/", loginController.post);
 
 module.exports = router;
