975188500cf674be6cd0bf05e08c42558b8bcb9b
eluka22 <eluka22@gmail.com>
2021-03-23 09:25:15 +0900

    Fix: authorization for login

--- routes/middlewares/authorization.js
+++ routes/middlewares/authorization.js
@@ -1,21 +1,10 @@
-const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
 
-const verifyToken = (req, res, next) => {
-  const cookie = req.cookies;
-
-  if (cookie.authcookie === undefined) {
-    res.redirect("/users/login");
-    return;
-  }
-
-  jwt.verify(cookie.authcookie, process.env.SECRET_KEY, (err, decoded) => {
-    if (err) {
-      next(err);
-      return;
-    }
-
+module.exports.isAuth = (req, res, next) => {
+  if (req.isAuthenticated()) {
     next();
-  });
-}
-
-exports.verifyToken = verifyToken;
+  } else {
+    // next(createError(401, { message: "unauthorized" }));
+    res.redirect("/users/login");
+  }
+};
