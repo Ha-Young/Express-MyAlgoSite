0109e16649038f9242de7e3aa86773a177c377a7
ha-young <hayeong28@naver.com>
2021-03-23 15:10:33 +0900

    add: User Model by using Joi, Joigoose, bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,31 @@
-const mongoose = require('mongoose');
+const Joi = require("joi");
+const Mongoose = require("mongoose");
+const Joigoose = require("joigoose")(Mongoose);
+const bcrypt = require("bcrypt");
 
-/*
+const SALT_ROUNDS = 10;
 
-  TODO: Fill in the model specification
-
- */
-const userSchema = new mongoose.Schema({
-
+const JoiUserSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  name: Joi.string().required(),
 });
 
-module.exports = mongoose.model('User', userSchema);
+const UserSchema = new Mongoose.Schema(Joigoose.convert(JoiUserSchema));
+
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  try {
+    const hashedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
+    user.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = Mongoose.model('User', UserSchema);
