bd64b1f04e172c878e0a4c39c2802d38ed4a9a55
onemo <wonmotime1@naver.com>
2021-03-23 23:12:40 +0900

    working on resultpage

--- routes/problem.js
+++ routes/problem.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Problem = require('../models/Problem');
 
-router.get('/:problem_id', async (req, res, next) => {
+router.get('/:problem_id', async (req, res) => {
   const problemId = req.params.problem_id;
   const problem = await Problem.find({ id: problemId });
   const {
@@ -14,10 +14,17 @@ router.get('/:problem_id', async (req, res, next) => {
 
   res.render('problem', {
     title,
+    problemId,
     completed_users,
     difficulty_level,
     description
   });
+});
+
+router.post('/:problem_id', (req, res) => {
+  const func = req.body.solution;
+
+  res.render('resultPage', { func });
 });
 
 module.exports = router;
