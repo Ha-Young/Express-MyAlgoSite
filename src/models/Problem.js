656a47dd752493832a39c1b6ac0eccc56f797e0b
ha-young <hayeong28@gmail.com>
2021-03-24 10:28:45 +0900

    add: problem schema in model - Problem

--- src/models/Problem.js
+++ src/models/Problem.js
@@ -1,12 +1,22 @@
-const mongoose = require('mongoose');
+const joi = require("joi");
+const mongoose = require("mongoose");
+const joigoose = require("joigoose")(mongoose);
 
-/*
-
-  TODO: Fill in the model specification
-
- */
-const ProblemSchema = new mongoose.Schema({
-
+const testSchema = joi.object().keys({
+  code: joi.string().required(),
+  solution: joi.string().required(),
 });
+
+const joiProblemSchema = joi.object({
+  id: joi.number().required(),
+  title: joi.string().required(),
+  completed_users: joi.number().required(),
+  difficulty_level: joi.number().required(),
+  description: joi.string(),
+  tests: joi.array().items(testSchema).required(),
+  img: joi.string(),
+});
+
+const ProblemSchema = new mongoose.Schema(joigoose.convert(joiProblemSchema));
 
 module.exports = mongoose.model('Problem', ProblemSchema);
