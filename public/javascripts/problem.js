bab6eb1a23c159ec6cdb4ec0bed9833b4372520d
ParkJeongbeen <pjb6510@gmail.com>
2021-03-24 15:38:15 +0900

    add problem's script

---
+++ public/javascripts/problem.js
@@ -0,0 +1,18 @@
+const codeMirror =
+  CodeMirror.fromTextArea(document.querySelector(".editor"), {
+    lineNumbers: true,
+    tabSize: 2,
+    mode: "javascript",
+    theme: "material",
+  });
+
+codeMirror.setSize("100%", 700);
+
+const problemListLinkButton = document.querySelector(".problem-list-link-button");
+const submitButton = document.querySelector(".submit-button");
+
+const handleProblemListLinkButtonClick = (e) => {
+  window.location.href = "/";
+}
+
+problemListLinkButton.addEventListener("click", handleProblemListLinkButtonClick);
