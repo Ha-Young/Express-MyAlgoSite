a3018ab8ae75a20f354206ad348ab576561c5f11
ha-young <hayeong28@gmail.com>
2021-03-24 14:33:13 +0900

    add: index page get problems and view"

--- src/routes/controllers/home.js
+++ src/routes/controllers/home.js
@@ -1,3 +1,11 @@
-exports.home = function (req, res, next) {
-  res.render('pages/index', { user: req.user || {} });
+const Problem = require("../../models/Problem");
+
+exports.home = async function (req, res, next) {
+  try {
+    const problems = await Problem.find().lean();
+
+    res.render('pages/index', { user: req.user || {}, problems });
+  } catch (err) {
+    next(err);
+  }
 };
