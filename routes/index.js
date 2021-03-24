ceb47a9cf71055a72208684ae6643b98718e20a2
Hansol Yun <hsyoon315@gmail.com>
2021-03-24 10:24:14 +0900

    Add problem list template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
 const router = express.Router();
+const problemsController = require("../routes/controllers/problems.controller");
+
+const { authenticateUser } = require("./middlewares/authorization");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: '바닐라코딩' });
+
+// router.get("/", authenticateUser, (req, res, next) => {
+//   res.render("index", { title: "vaco" });
+// });
+
+router.get("/", authenticateUser, problemsController.getProblems);
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
 });
+
+router.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] }
+));
+
+router.get("/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }
+));
 
 module.exports = router;
