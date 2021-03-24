fb146272678031205e997d80dd3016f8f267b7b0
kwon, minho <minhob38@gmail.com>
2021-03-24 21:22:31 +0900

    Implement Error Handling

--- routes/problems.js
+++ routes/problems.js
@@ -6,35 +6,44 @@ const getArgsAndBody = require("../utils/getArgsAndBody");
 
 //Q. DB는 미들웨어 or 라우터마다 호출?
 router.get("/", async (req, res, next) => {
-  const problems = await Problem.find({});
-  res.render("problems", { problems: problems });
+  try {
+    const problems = await Problem.find({});
+    res.render("problems", { problems: problems });
+  } catch (err) {
+    next(createError(404, "there are problems :("));
+  }
 });
 
 router.get("/:problem_id", async (req, res, next) => {
-  const { problem_id } = req.params;
-  // util 함수로 만들것!
-  const problems = await Problem.find({});
+  try {
+    const problems = await Problem.find({});
+    const { problem_id } = req.params;
+    // util 함수로 만들것!
+    const problemIdx = problems.findIndex((problem) => {
+      return problem.id.toString() === problem_id;
+    });
 
-  const problemIdx = problems.findIndex((problem) => {
-    return problem.id.toString() === problem_id;
-  });
+    // if (problemIdx === -1) {
+    //   return next(createError(404, "This Problem Does Not Exist :("));
+    // }
 
-  // next() return도 될듯???
-  if (problemIdx === -1) {
-    return next(createError(404, "This Problem Does Not Exist :("));
+    res.render("problem", { problem: problems[problemIdx] });
+  } catch (err) {
+    next(createError(404, "There is no problem :("));
   }
-
-  res.render("problem", { problem: problems[problemIdx] });
 });
 
 router.post("/:problem_id", (req, res, next) => {
   const { solution } = req.body;
-  const { args, body } = getArgsAndBody(solution);
 
-  // error 처리할 것...
-  const solutionFunc = new Function(...args, body);
-  solutionFunc();
-  res.send("POST 구현해야 합니다.");
+  try {
+    const { args, body } = getArgsAndBody(solution);
+    const solutionFunc = new Function(...args, body);
+    solutionFunc();
+    res.send("POST 구현해야 합니다.");
+  } catch (err) {
+    res.send("함수를 입력하세요.");
+  }
 });
 
 module.exports = router;
--- utils/getArgsAndBody.js
+++ utils/getArgsAndBody.js
@@ -5,13 +5,17 @@ const getArgsAndBody = (func) => {
   const reOfArgsBracket = /\([^\)]*\)/;
   const reOfBodyBracket = /\{.*\}/;
 
-  const argsBracket = trimmedFuncStr.match(reOfArgsBracket)[0];
-  const bodyBracket = trimmedFuncStr.match(reOfBodyBracket)[0];
+  try {
+    const argsBracket = trimmedFuncStr.match(reOfArgsBracket)[0];
+    const bodyBracket = trimmedFuncStr.match(reOfBodyBracket)[0];
 
-  const args = argsBracket.slice(1, -1).split(",");
-  const body = bodyBracket.slice(1, -1);
+    const args = argsBracket.slice(1, -1).split(",");
+    const body = bodyBracket.slice(1, -1);
 
-  return { args, body };
+    return { args, body };
+  } catch (err) {
+    return err;
+  }
 };
 
 module.exports = getArgsAndBody;
