b0bd11bf2685b21e7fcd133b0b3417a71eab4cad
onemo <wonmotime1@naver.com>
2021-03-24 14:35:18 +0900

    add function execution in router

--- routes/problem.js
+++ routes/problem.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Problem = require('../models/Problem');
+const vm = require('vm');
 
 router.get('/:problem_id', async (req, res) => {
   const problemId = req.params.problem_id;
@@ -21,10 +22,32 @@ router.get('/:problem_id', async (req, res) => {
   });
 });
 
-router.post('/:problem_id', (req, res) => {
+router.post('/:problem_id', async (req, res) => {
   const func = req.body.solution;
+  const problemId = req.params.problem_id;
+  const problem = await Problem.find({ id: problemId });
+  const tests = problem[0].tests;
+  const sandbox = {
+    result: 0
+  };
+  // const code = func + "result = solution(5)";
+  // const context = new vm.createContext(sandbox);
+  // const script = new vm.Script(code);
 
-  res.render('resultPage', { func });
+  // try {
+
+    for (let i = 0; i < tests.length; i ++) {
+      const code = func + `result = ${tests[i].code}`;
+      const context = new vm.createContext(sandbox);
+      const script = new vm.Script(code);
+
+      script.runInContext(context);
+
+      console.log(sandbox.result);
+      console.log(tests[i].solution);
+    }
+  // } catch (err) {
+  // }
 });
 
 module.exports = router;
