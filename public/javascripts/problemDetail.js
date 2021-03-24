364280455c795b2a1b6b4296fa4f17ca87382c85
Jay_WooKJ <natcho9010@gmail.com>
2021-03-24 17:01:14 +0900

    add: codemirror addons

--- public/javascripts/problemDetail.js
+++ public/javascripts/problemDetail.js
@@ -2,5 +2,13 @@ CodeMirror.fromTextArea(document.getElementById('textEditor'), {
   lineNumbers: true,
   tabSize: 2,
   mode: "javascript",
-  theme: "material"
+  theme: "material",
+  autoCloseBrackets: true,
+  matchBrackets: true,
+  matchTags: true,
+  autoCloseTags: true,
+  showTrailingSpace: true,
+  editor: {
+    showHint: true
+  }
 });
