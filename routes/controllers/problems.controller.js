a1c9f3ce13b8215fc4fa9ad032838f9562dad8d0
hyeongju <kokiguitars@gmail.com>
2021-03-24 19:00:03 +0900

    Add vm to execute user written code

--- routes/controllers/problems.controller.js
+++ routes/controllers/problems.controller.js
@@ -1,27 +1,45 @@
 const assert = require("assert");
-const {VM, VMScript} = require("vm2");
+const vm = require("vm");
 const Problem = require("../../models/Problem");
 
 exports.getProblem = async function (req, res, next) {
   const problemId = req.params.problem_id;
   const problem = await Problem.find({id: problemId});
   const parameters = problem[0].parameters.join(", ");
-  const firstLine = "function(" + parameters + ") {}";
-  
+  const firstLine = "function solution (" + parameters + ") {}";
+
   res.render("problem", { problem: problem[0], firstLine: firstLine});
 }
 
-exports.postSolution = function (req, res,next) {
-  const sandbox = {
-    result : {}
+exports.postSolution = async function (req, res,next) {
+  const problemId = req.params.problem_id;
+  const problem = await Problem.find({id: problemId});
+  const testcases = problem[0].tests;
+
+  for (let i = 0; i < testcases.length; i++) {
+    const solution = req.body.solution;
+    const code = testcases[i].code;
+    const answer = testcases[i].solution;
+
+    
+    try {
+      const sandbox = {
+        result: ""
+      }
+      const context = vm.createContext(sandbox);
+      const script = new vm.Script(solution + "result=" + code);
+      console.log(script);
+      script.runInContext(context);
+
+      console.log(sandbox.result, answer);
+    } catch (error) {
+      res.render("problemError", {
+        name: error.name,
+        message: error.message,
+        stack: error.stack
+      });
+      return;
+    }
   }
-
-  const vm = new VM(sandbox);
-
-  const solution = "result = " + req.body.solution;
-  const script = new VMScript(solution);
-  vm.run(script);
-
-  console.log(vm.sandbox.result);
   res.render("success");
 }
