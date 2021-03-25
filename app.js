2aa27baff3dc379b719a9d11dd25296d3320d400
hyeongju <kokiguitars@gmail.com>
2021-03-25 16:53:42 +0900

    Remove comments

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ db.once("open", function () {
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store')
   next()
-})
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -36,10 +36,8 @@ app.set("view engine", "ejs");
 
 app.use(passport.initialize());
 
-
 const session = require("express-session");
 app.use(passport.session());
-
 app.use(session({
   secret: "hyeongju",
   resave: false,
@@ -59,11 +57,9 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
