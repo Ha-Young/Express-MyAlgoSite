0746ae50d8308b1b5e4fc989e2bf08158bb6ba8a
JUNGJUHYOUNG <fheh1992@gmail.com>
2021-03-25 12:57:48 +0900

    Work on checkUserSolution

--- routes/controllers/problemController.js
+++ routes/controllers/problemController.js
@@ -1,7 +1,10 @@
 const Problem = require("../../models/Problem");
+const { VM } = require("vm2");
 const vm = require("vm");
 
 exports.renderProblemPageById = async function (req, res, next) {
+  console.log("global", global);
+
   const requestedId = req.params.id;
   const fetchedProblem = await Problem.findById(requestedId);
 
@@ -9,35 +12,38 @@ exports.renderProblemPageById = async function (req, res, next) {
 };
 
 exports.checkUserSolution = async function (req, res, next) {
-  const sandbox = { solution: null };
+  const vm2 = new VM({
+    sandbox: { solution: null },
+  });
 
-  vm.createContext(sandbox);
-  const submitedCode = vm.runInNewContext(
-    `solution = ${req.body.solution}`,
-    sandbox
-  );
+  const submitedCode = vm2.run(`solution = ${req.body.solution}`);
+  console.log(submitedCode);
 
+  const submitedCode2 = vm2.run(`solution(['Jane', 'Kim'])`);
+  console.log(submitedCode2);
+
+  // const sandbox = { solution: null };
+
+  // vm.createContext(sandbox);
   // const submitedCode = vm.runInNewContext(
   //   `solution = ${req.body.solution}`,
   //   sandbox
   // );
 
-  // console.log(submitedCode(4));
-  // console.log(sandbox.solution(1));
+  // const submitedCode2 = vm.runInNewContext(
+  //   `solution(['Jane', 'Kim'])`,
+  //   sandbox
+  // );
 
-  const submitedCode2 = vm.runInNewContext(
-    `solution(['Jane', 'Kim'])`,
-    sandbox
-  );
+  // console.log(submitedCode2);
 
-  console.log(submitedCode2);
+  // const submitedCode3 = vm.runInNewContext(
+  //   `solution(['Jane', 'Huh', 'Kim'])`,
+  //   sandbox
+  // );
 
-  const submitedCode3 = vm.runInNewContext(
-    `solution(['Jane', 'Huh', 'Kim'])`,
-    sandbox
-  );
-
-  console.log(submitedCode3);
+  // console.log(submitedCode3);
+  // console.log(sandbox);
 
   const requestedId = req.params.id;
   const fetchedProblem = await Problem.findById(requestedId);
