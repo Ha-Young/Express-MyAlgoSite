2d9e00afb0ecef68de9f237b931514edee55933f
ha-young <hayeong28@naver.com>
2021-03-26 01:43:57 +0900

    add: problem submit answer process

--- public/javascripts/templates/problemTemplate.js
+++ public/javascripts/templates/problemTemplate.js
@@ -1,41 +1,74 @@
 /* eslint-disable indent */
 function testCaseResultTableTemplate(testCaseResult) {
-  return `<table class="console-testcase" data-index="0">
-    <tbody>
-      <tr>
-        <td colspan="2">테스트 ${testCaseResult.index} </td>
-      </tr>
-      <tr>
-        <td class="td-label" align="right">케이스 <span>〉</span></td>
-        <td class="input">${testCaseResult.code}</td>
-      </tr>
-      <tr>
-        <td class="td-label" align="right">기댓값 <span>〉</span></td>
-        <td class="output">${testCaseResult.solution}</td>
-      </tr>
-      <tr>
-        <td class="td-label" align="right" valign="top">실행 결과 <span>〉</span></td>
+  let outputStr = "";
+
+  if (testCaseResult.outputs.length > 0) {
+    for (const output of testCaseResult.outputs) {
+      outputStr += output + '<br>';
+    }
+  }
+
+  return `
+    <table class="console-testcase" data-index="0">
+      <tbody>
+        <tr>
+          <td colspan="2">테스트 ${testCaseResult.index} </td>
+        </tr>
+        <tr>
+          <td class="td-label" align="right">케이스 <span>〉</span></td>
+          <td class="input">${testCaseResult.code}</td>
+        </tr>
+        <tr>
+          <td class="td-label" align="right">기댓값 <span>〉</span></td>
+          <td class="output">${testCaseResult.solution}</td>
+        </tr>
+        <tr>
+          <td class="td-label" align="right" valign="top">실행 결과 <span>〉</span></td>
+          ${
+            testCaseResult.isCollect
+              ? `<td class="result passed">${testCaseResult.msg}</td>`
+              : `<td class="result failed">${testCaseResult.msg}</td>`
+          }
+        </tr>
         ${
-          testCaseResult.isCollect
-            ? `<td class="result passed">${testCaseResult.msg}</td>`
-            : `<td class="result failed">${testCaseResult.msg}</td>`
+          outputStr &&
+            `<tr>
+              <td class="td-label" align="right">출력값 <span>〉</span></td>
+              <td class="output">${outputStr}</td>
+            </tr>`
         }
-      </tr>
-    </tbody>
-  </table>`;
+      </tbody>
+    </table>`;
 }
 
 function getTestCaseSolveTemplates(solveResult) {
-  let templateStr = "";
-  console.log(solveResult);
-
-  for (const caseResult of solveResult.caseResultList) {
-    templateStr += testCaseResultTableTemplate(caseResult);
-  }
-
-  console.log(templateStr);
+  const templateStr = solveResult.caseResultList.reduce((acc, cur) => {
+    return acc + testCaseResultTableTemplate(cur);
+  }, "");
 
   return templateStr;
 }
 
-function getAnswerSolveTemplates(solveResult) {}
+function answerResultTableTemplate(answerResult) {
+  return `
+    <table class="console-answer" data-category="correctness">
+      <tbody>
+        <tr>
+          <td valign="top" class="td-label">테스트 ${answerResult.index} <span>〉</span></td>
+          ${
+            answerResult.isCollect
+              ? `<td class="result passed">${answerResult.summary}</td>`
+              : `<td class="result failed">${answerResult.summary}</td>`
+          }
+        </tr>
+      </tbody>
+    </table>`;
+}
+
+function getAnswerSolveTemplates(solveResult) {
+  const templateStr = solveResult.caseResultList.reduce((acc, cur) => {
+    return acc + answerResultTableTemplate(cur);
+  }, "");
+
+  return templateStr;
+}
