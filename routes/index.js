4cefda7f0a9156567d08a090492c47b1747176fd
kwon, minho <minhob38@gmail.com>
2021-03-25 17:20:19 +0900

    Implement index style

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const problems = require("../models/sample_problems.json");
 
-/* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { problems: problems });
+  res.render("index");
 });
 
 module.exports = router;
