fa340f6f55a67223f4f56858107965a3b91d4816
ParkJeongbeen <pjb6510@gmail.com>
2021-03-26 01:52:05 +0900

    add solvedProblems

--- controller/problems.controller.js
+++ controller/problems.controller.js
@@ -12,25 +12,30 @@ const vm = new VM({
   timeout: 5000,
 });
 
-
 exports.get = async (req, res, next) => {
   try {
     const problemId = req.params.problem_id;
     const user = await req.user;
     const problem = await getProblemById(problemId);
-    const flashContents = req.flash("result")[0];
-
-    console.log(flashContents);
+    const flashContent = req.flash("codes")[0];
+    const triedProblem = user.problems.find(
+      (problem) => problem.problemId.toString() === problemId
+    );
 
     if (!problem) {
       next(createError(400, "invalid problem id"));
       return;
     }
 
+    const isSolved = triedProblem ? triedProblem.isSolved : false;
+
     res.render("problem", {
       email: user.email,
       problem,
-      result: flashContents,
+      isSolved,
+      codes:
+        flashContent ||
+          { code: triedProblem ? triedProblem.code : "function solution(arg){\n  // Your code...\n}", log: [] },
     });
   } catch (err) {
     next(createError(500, err.message));
@@ -49,6 +54,7 @@ exports.submit = async (req, res, next) => {
     }
 
     let testResult = true;
+    let functionCallMessage = "";
     try {
       const tests = currentProblem.tests;
       vm.run(`submitted.log = []`);
@@ -60,32 +66,58 @@ exports.submit = async (req, res, next) => {
           }
         };
       `);
-      vm.run(`submitted.solution = ${code}`);
-
       for (const test of tests) {
-        vm.run(`submitted.log = []`);
-        const result = vm.run(`submitted.${test.code}`);
+        functionCallMessage = test.code;
+        const result = vm.run(`${code} ${test.code}`);
         assert.deepStrictEqual(result, test.solution);
+
+        submitted.log.push("-----------------");
       }
     } catch (err) {
       testResult = false;
-      req.flash("result", {
-        result: `${err.name} : ${stripAnsi(err.message)}`,
+      req.flash("codes", {
+        result: `>> ${functionCallMessage}\n${err.name} : ${stripAnsi(err.message)}\n`,
         log: submitted.log,
         code,
       });
     }
 
+
+    const problem = {
+      title: currentProblem.title,
+      problemId,
+      isSolved: testResult,
+      code,
+    };
+    const user = await req.user;
+    const triedProblem = user.problems.find(
+      (problem) => problem.problemId.toString() === problemId
+    );
+
+    const hasAlreadySolved = triedProblem ? triedProblem.isSolved : false;
+
     if (testResult) {
-      req.flash("result", {
-        result: "Success!",
+      if (!hasAlreadySolved) {
+        currentProblem.completed_users += 1;
+        await currentProblem.save();
+      }
+
+      await user.updateOne({ $pull: { problems: { problemId, } } });
+      await user.updateOne({ $push: { problems: problem } });
+
+      req.flash("codes", {
+        result: "Success! Congratulations!!",
         log: submitted.log,
         code,
       });
+    } else {
+      if (!hasAlreadySolved) {
+        await user.updateOne({ $pull: { problems: { problemId, } } });
+        await user.updateOne({ $push: { problems: problem } });
+      }
     }
     res.redirect(`/problems/${problemId}`);
   } catch (err) {
     next(createError(500, err.message));
   }
 };
-
--- models/User.js
+++ models/User.js
@@ -5,10 +5,32 @@ const bcrypt = require("bcrypt");
   TODO: Fill in the model specification
 
  */
+
+
+const solvedProblemSchema = new mongoose.Schema({
+  problemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Problem",
+    require: true,
+  },
+  title: {
+    type: String,
+  },
+  isSolved: {
+    type: Boolean,
+    require: false,
+  },
+  code: {
+    type: String,
+    trim: true,
+  },
+});
+
 const userSchema = new mongoose.Schema({
   name: { type: String, maxlength:20, require: true },
-  email: { type: String, require: true, unique: true },
+  email: { type: String, require: true, unique: true, index: true, },
   password: { type: String, require: true },
+  problems: [solvedProblemSchema],
 });
 
 const saltRounds = 10;
