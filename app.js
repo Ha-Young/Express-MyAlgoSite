e8be76dcec8b3ed36d05ec59de9c32586cf2ce18
seongjuseo <canwefly89@gmail.com>
2021-03-26 04:26:34 +0900

    heroku testing

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
 app.use(flash());
