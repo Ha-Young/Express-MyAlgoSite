555f9b51fb7a339f91b72d8a21b46226ae5f3d21
wonho <biocle8339@gmail.com>
2021-03-25 09:44:45 +0900

    EDIT: view problemBlock, problemBlock css

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -2,10 +2,14 @@ const Problem = require('../models/Problem');
 const catchAsync = require('../middlewares/catchAsync');
 const generateHeaderData = require('../utils/generateHeaderData');
 
-exports.getHome = catchAsync(async (req, res, next) => {
+exports.getHome = catchAsync(async (req, res) => {
   const headerData = generateHeaderData(req.isAuthenticated(), req.user);
   const problems = await Problem.find().lean();
+  const problemData = problems.map(problem => {
+    const problemRef = `/problems/${problem.id}`;
+    return { problemRef, ...problem };
+  });
 
-  res.render('index', { problems, ...headerData });
+  res.render('index', { problemData, ...headerData });
 });
 
