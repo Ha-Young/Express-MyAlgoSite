2a0794dff83e7f893c680cbccd5eac0b4576e79c
minwoo kim <vvee1253@gmail.com>
2021-03-26 19:38:13 +0900

    Fix index router spacing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+
 const { checkAuthenticated } = require("../middlewares/auth");
+
 const router = express.Router();
 
 router.use("/auth", require("./auth"));
+
 router.use(checkAuthenticated);
+
 router.use("/", require("./home"));
 router.use("/problems", require("./problems"));
 
