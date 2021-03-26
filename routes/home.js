f7ecf1bb9ef49b116c6ddb47f92202474334f273
hyerichung <hyerichungonearth@gmail.com>
2021-03-26 21:26:15 +0900

    Fix typo

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const verifyAuth = require("../middlewares/verifyAuth");
 const problemController = require("../controllers/problemController");
