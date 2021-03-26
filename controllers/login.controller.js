82930bb1ca467e69275afc7f9669a795e05ba12b
Jay_WooKJ <natcho9010@gmail.com>
2021-03-26 15:53:58 +0900

    delete: login, join

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,17 +1,5 @@
 const passport = require("passport");
 
-module.exports.loginController = function loginController(req, res, next) {
-  res.render("login", { title: "Sign In" });
-}
-
-module.exports.postLoginController = function postLoginController(req, res, next) {
-  const {
-    body: { email, password }
-  } = req;
-
-  next();
-}
-
 const getGoogleController = passport.authenticate("google", { scope: ["profile", "email", "openid"] });
 
 module.exports.getGoogleController = getGoogleController;
--- routes/globalRouter.js
+++ routes/globalRouter.js
@@ -1,19 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { mainController } = require("../controllers/main.controller");
-const { loginController, postLoginController, getGoogleController, getGoogleCallbackController, logoutContorller } = require("../controllers/login.controller");
-const { joinController, postJoinController } = require("../controllers/join.controller");
+const { getGoogleController, getGoogleCallbackController, logoutContorller } = require("../controllers/login.controller");
 const { redirectToHome } = require("../middlewares");
 
 router.get("/", mainController);
 
-router.get("/login", loginController);
-router.post("/login", postLoginController);
 router.get("/logout", logoutContorller);
 router.get("/login/google", getGoogleController);
 router.get("/login/google/callback", getGoogleCallbackController, redirectToHome);
-
-router.get("/join", joinController);
-router.post("/join", postJoinController);
 
 module.exports = router;
