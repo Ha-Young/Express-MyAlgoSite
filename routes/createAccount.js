1ee3cdf72e9b4dfe16df6408d09de7d15464525c
ParkJeongbeen <pjb6510@gmail.com>
2021-03-26 16:51:56 +0900

    remove unnecessary line break

--- routes/createAccount.js
+++ routes/createAccount.js
@@ -1,9 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
+
 const createAccountController = require("../controller/createAccount.controller");
-
-
 
 router.get("/", createAccountController.get);
 
