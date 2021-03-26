748bf697c0253a2c25654a4ac7c6a14abfa6e2a5
Hansol Yun <hsyoon315@gmail.com>
2021-03-26 17:25:32 +0900

    Add configs module

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  serverAddress: process.env.SERVER_ADDRESS,
+  password: process.env.PASSWORD,
+  secretCode: process.env.SECRET_CODE,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+};
---
+++ config/passport.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const User = require("../models/User");
+const configs = require(".");
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: configs.googleClientId,
+    clientSecret: configs.googleClientSecret,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    await User.findOrCreate({
+      google_id: profile.id,
+      email: profile.emails[0].value,
+      username: profile.displayName,
+      photo_url: profile.photos[0].value,
+    }, (err, user) => {
+      user.save();
+      return done(null, user);
+    });
+  }));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
