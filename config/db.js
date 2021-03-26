6ec97e72d9a72746f970c559654a8cb9f880f97d
minwoo kim <vvee1253@gmail.com>
2021-03-26 20:04:21 +0900

    Add dangling comma to db config

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 function initialize() {
