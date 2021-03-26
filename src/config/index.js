5171878d66dae7ee7142b1bd2bb6b8d0b1b54346
ha-young <hayeong28@naver.com>
2021-03-26 19:20:26 +0900

    fix: .env err check remove

--- src/config/index.js
+++ src/config/index.js
@@ -5,10 +5,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 
-if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
-}
+// if (envFound.error) {
+//   // This error should crash whole process
+//   throw new Error("⚠️  Couldn't find .env file  ⚠️");
+// }
 
 module.exports = {
   port: normalizePort(process.env.PORT || '3000'),
