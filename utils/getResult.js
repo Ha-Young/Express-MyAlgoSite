14753e4e456828f99adadfebe682eb97f9ab049d
Hansol Yun <hsyoon315@gmail.com>
2021-03-26 17:39:24 +0900

    Add get solution result function

---
+++ utils/getResult.js
@@ -0,0 +1,45 @@
+const vm = require("vm");
+
+function getResult(userCode, problem) {
+  try {
+    const resultList = [];
+    const context = { result: 0 };
+
+    vm.createContext(context);
+
+    for (let i = 0; i < problem.tests.length; i++) {
+      const code = userCode + "result=" + problem.tests[i].code;
+      const script = new vm.Script(code);
+
+      script.runInContext(context);
+
+      const result = {
+        testResult: context.result,
+        expectedResult: problem.tests[i].solution,
+      };
+
+      if (result.testResult === result.expectedResult) {
+        result.isPassed = true;
+      } else {
+        result.isFailed = true;
+      }
+
+      resultList.push(result);
+    }
+
+    const isAllPassed = resultList.every(result => result.isPassed);
+
+    return {
+      isAllPassed,
+      resultList: resultList,
+      hasSolutionError: false
+    }
+  } catch (err) {
+    return {
+      errorMessage: err.message,
+      hasSolutionError: true
+    }
+  }
+}
+
+module.exports = getResult;
