7675a0ad2284736193e2a69e05862eb3ed775096
Hansol Yun <hsyoon315@gmail.com>
2021-03-26 17:26:22 +0900

    Delete file

--- routes/middlewares/authorization.js
+++
@@ -1,9 +0,0 @@
-function authenticateUser(req, res, next) {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(301).redirect("/login");
-  }
-}
-
-exports.authenticateUser = authenticateUser;
