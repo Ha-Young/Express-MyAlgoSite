94bfbe21e2c5e792d740a8376a0e6bf27e9c2ae8
Hansol Yun <hsyoon315@gmail.com>
2021-03-26 17:23:21 +0900

    Move passport to new file

--- app.js
+++ app.js
@@ -3,53 +3,24 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const session = require("express-session");
+const createError = require("http-errors");
+const configs = require("./config");
+
+require("./config/passport");
 
 const mongoose = require("mongoose");
-const SERVER_ADDRESS = process.env.SERVER_ADDRESS;
-const PASSWORD = process.env.PASSWORD;
-const DB = SERVER_ADDRESS.replace("<password>", PASSWORD);
+const DB = configs.serverAddress.replace("<password>", configs.password);
 
-mongoose.connect(DB,
-  { useNewUrlParser: true,
+mongoose.connect(DB, {
+    useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(() => console.log("server connect!!"));
 
 const db = mongoose.connection;
+
 db.on("error", () => console.log("error"));
 db.once("open", () => console.log("connect!!!"));
-
-const User = require("./models/User");
-
-passport.use(new GoogleStrategy(
-  {
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
-    passReqToCallback: true,
-  },
-  (request, accessToken, refreshToken, profile, done) => {
-    User.findOrCreate({
-      googleId: profile.id,
-      email: profile.emails[0].value,
-      username: profile.displayName,
-      photo_url: profile.photos[0].value,
-      solved_count: 0,
-    }, (err, user) => {
-      user.save();
-
-      return done(null, user);
-    });
-  }));
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
 
 const index = require("./routes/index");
 const problems = require("./routes/problems");
@@ -64,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(session({
-  secret: process.env.SECRET_CODE,
+  secret: configs.secretCode,
   cookie: { maxAge: 60 * 60 * 1000 },
   resave: true,
   saveUninitialized: false,
@@ -78,12 +49,9 @@ app.use("/problems", problems);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+  next(createError(404, "Not Found"));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
