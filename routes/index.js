3a24cca6f294b256b108f015f620b7bbde625d49
minwoo kim <vvee1253@gmail.com>
2021-03-26 19:34:40 +0900

    Delete unused comment

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,6 @@ router.use(function(req, res, next) {
 
 // error handler
 router.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
