fa16acfef619ac0627de45fd56c6458a16c0a578
Jay_WooKJ <natcho9010@gmail.com>
2021-03-26 15:37:32 +0900

    fix: dummydata

--- routes/problemsRouter.js
+++ routes/problemsRouter.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { problemDetailController, postProblemDetailController } = require("../controllers/problem.controller");
 const { onlyAuthorizedAllowed } = require("../middlewares");
-const Problem = require("../models/Problem");
-const dummy = require("../dummydata");
 
 router.get("/:problem_id", onlyAuthorizedAllowed, problemDetailController);
 router.post("/:problem_id", onlyAuthorizedAllowed, postProblemDetailController);
