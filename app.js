aeb0700482b99fab3bfed4c5aa77446ce975a94c
ParkJeongbeen <pjb6510@gmail.com>
2021-03-26 16:51:03 +0900

    remove code description

--- app.js
+++ app.js
@@ -19,11 +19,6 @@ const checkAuth = require("./middlewares/checkAuthenticated");
 
 const app = express();
 
-/*
-  dbUser / vanillacoding
-  mongodb+srv://dbUser:vanillacoding@cluster0.hugiy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-*/
-
 mongoose.connect(
   process.env.DB_ADDRESS,
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }
@@ -61,20 +56,16 @@ app.use("/create_account", checkAuth.checkNotAuthenticated, createAccount);
 app.use("/problems", problems);
 app.use("/", checkAuth.checkAuthenticated, index);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
