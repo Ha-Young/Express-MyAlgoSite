5acf3251309ac9f8a0c45c59dab9d758ef02a59b
hyerichung <hyerichungonearth@gmail.com>
2021-03-27 19:01:12 +0900

    Add proxy option

--- config/passport/googlePassport.js
+++ config/passport/googlePassport.js
@@ -5,7 +5,8 @@ const User = require("../../models/User");
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/login/google/callback"
+  callbackURL: "/login/google/callback",
+  proxy: true,
 },
   async (accessToken, refreshToken, profile, done) => {
     const { id, displayName } = profile;
