ced0008ec62013f72efc9f393ad3e1072d8c497e
KimJaedeok <64330699+jdcoder27@users.noreply.github.com>
2021-03-27 20:27:44 +0900

    modify adress

--- passport.js
+++ passport.js
@@ -7,7 +7,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `http://localhost:3000/login/google/callback`
+    callbackURL: "https://fierce-badlands-40150.herokuapp.com/login/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     const isUser = await User.exists({ googleId: Number(profile.id) });
