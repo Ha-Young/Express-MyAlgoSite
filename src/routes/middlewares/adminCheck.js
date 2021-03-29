fd94d51ed9dadec0588eb0ae12d9712a5250f8d2
ha-young <hayeong28@naver.com>
2021-03-29 13:47:01 +0900

    add: problemCreate controller, front-simpleMDE

--- src/routes/middlewares/adminCheck.js
+++ src/routes/middlewares/adminCheck.js
@@ -1,3 +1,9 @@
+const createErr = require("http-errors");
+
 module.exports = function adminCheck(req, res, next) {
-  console.log(req.user);
+  if (!req.user.isAdmin) {
+    next(createErr(401));
+  }
+
+  next();
 };
